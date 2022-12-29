c6c8350eaa2f107c3d49970cfdf3c0c46ace0c17
Егор Люханов <ealuhanov@yandex.ru>
2022-12-29 17:41:58 +0500

    Добавьте файлы проекта.

---
+++ InstanceThreadPool.ConsoleTest/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace InstanceThreadPool.ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
